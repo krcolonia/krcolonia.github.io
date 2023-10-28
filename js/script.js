489ad454d6ab23cee1520653729ec3dd9935fbf6
Kurt Colonia <kurtrobinperezcolonia@gmail.com>
2023-10-28 20:25:30 +0800

    9th Commit: Design Changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-
-
 function dropDownFunc() {
-    var menu = document.querySelector(".headerDropdown");
+  var menu = document.querySelector(".headerDropdown");
 
-    if(menu.style.top === "0px" || menu.style.top === "") {
-        menu.style.top = "55px";
-    }
-    else {
-        menu.style.top = "0px";
-    }
+  if(menu.style.top === "0px" || menu.style.top === "") {
+    menu.style.top = "55px";
+  }
+  else {
+    menu.style.top = "0px";
+  }
 }
