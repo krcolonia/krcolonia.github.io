2ecec47388e34aa2f6ef9490603bd552a4caa262
Kurt Colonia <kurtrobinperezcolonia@gmail.com>
2023-10-31 23:55:39 +0800

    11th Commit: Added some design to the site title.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 function dropDownFunc() {
   var menu = document.querySelector(".headerDropdown");
 
-  if(menu.style.top === "0px" || menu.style.top === "") {
-    menu.style.top = "55px";
+  if(window.innerWidth <= 768) {
+    if(menu.style.top === "0px" || menu.style.top === "") {
+      menu.style.top = "55px";
+    }
+    else {
+      menu.style.top = "0px";
+    }
   }
-  else {
-    menu.style.top = "0px";
+}
+
+var headStr = "<krColonia>";
+let headIndx = 0;
+
+function printHomeHeader() {
+  if(headIndx < headStr.length){
+    document.getElementById("headerContent").innerHTML += headStr[headIndx];
+    headIndx++;
+    setTimeout(printHomeHeader, 150);
   }
 }
