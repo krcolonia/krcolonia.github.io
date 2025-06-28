738b663d11d163dfce82785ab47804cf30eef360
Kurt Colonia <krcolonia@gmail.com>
2025-06-28 11:05:20 +0800

    Added functionality to sidebar buttons to make them add tabs within the main area of the site

--- js/newScript.js
+++ js/newScript.js
@@ -1,14 +1,13 @@
-const sidebarContainer = document.getElementById('sidebarContainer');
-const sidebar = document.getElementById('sidebarNav');
-const sidebarBtnIcon = document.getElementById('sidebarBtnIcon');
-
 var sidebarOpen = false;
 
 function toggleSidebar() {
-	// console.log('aooga moogada tubaki')
+	const sidebarContainer = document.getElementById('sidebarContainer');
+	const sidebar = document.getElementById('sidebarNav');
+	const sidebarBtnIcon = document.getElementById('sidebarBtnIcon');
+
 	if(sidebarOpen == true) {
-		sidebarContainer.style.width = '50px';
-		sidebar.style.left = '-200px';
+		sidebarContainer.style.width = '40px';
+		sidebar.style.left = '-210px';
 		sidebarBtnIcon.src = 'images/sidebar-open.png';
 		sidebarOpen = false;
 	}
@@ -20,3 +19,15 @@ function toggleSidebar() {
 	}
 }
 
+var currentTabs = [];
+
+function addTab(tabName) {
+	const tabContainer = document.getElementById('tabContainer');
+
+	if (currentTabs.includes(tabName)) {
+		return; // ? guard clause to check if the tab was already added
+	}
+
+	tabContainer.innerHTML += `<div class="menuTab d-flex flex-row justify-content-between align-content-center bg-black text-white p-1"><span>${tabName}</span><button>x</button></div>`;
+	currentTabs.push(tabName);
+}
