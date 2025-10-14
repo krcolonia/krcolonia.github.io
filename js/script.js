e6b89af079810297095233e00633799a1f51758a
krcolonia <krcolonia@gmail.com>
2025-10-14 21:22:38 +0800

    temporarily disabling typing animations for easier debugging

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ function setCurrentTab(tabName) {
 //#region // ? functions for changing the content of body
 function clearTabContent(headers) {
 	headers.forEach(id => {
-		console.log(id);
+		// console.log(id);
 		const el = document.getElementById(id);
 		if(el) {
 			el.innerHTML = '';
@@ -191,6 +191,10 @@ async function setBodyContent(tabName) {
 	// clearTabContent();
 
 	let typeSpeed; // ? The lower the typeSpeed value, the faster the type effect goes
+
+	let isTyping = false;
+	// let isTyping = document.getElementById("typeAnim").checked;
+	// console.log(isTyping);
 
 	switch(tabName) {
 		case "Home":
@@ -406,15 +410,225 @@ async function setBodyContent(tabName) {
 					await safeType('tsVersionControlSection', `
 			// ? Version Control
 			`, 'aboutCursor', typeSpeed);
-					await safeType('tsGit', `"Git"`, 'aboutCursor', typeSpeed); await safeType('tsGitComma', `, `, 'aboutCursor', typeSpeed);
+					await safeType('tsGit', `"Git (Git Bash)"`, 'aboutCursor', typeSpeed); await safeType('tsGitComma', `, `, 'aboutCursor', typeSpeed);
 					await safeType('tsGithub', `"Github"`, 'aboutCursor', typeSpeed); await safeType('tsGithubComma', `,
-		`, 'aboutCursor', typeSpeed);
+	`, 'aboutCursor', typeSpeed);
 
 					await safeType('tsCloseBracket', '}', 'aboutCursor', typeSpeed);
 					await safeType('tsDelimiter', ';', 'aboutCursor', typeSpeed);
 				}
 			}
-			await aboutMeContent();
+
+			function aboutMeContentNoAnim() {
+				console.log('dont type this just print');
+
+				const codeInsert = document.getElementById('aboutMainFunc');
+
+				if(!document.getElementById('nameDelimit')) {
+					codeInsert.innerHTML += `<span class="code-keyword" id="nameKeyword"></span><span class="code-variable" id="nameVar"></span><span id="nameEqual"></span><span class="code-string" id="nameString"></span><span id="nameDelimit"></span>`;
+				}
+
+				if(document.getElementById('nameDelimit').innerHTML.trim() == '') {
+					clearTabContent([
+						'nameKeyword',
+						'nameVar',
+						'nameEqual',
+						'nameString',
+						'nameDelimit'
+					]);
+					document.getElementById('nameKeyword').innerHTML = 'String ';
+					document.getElementById('nameVar').innerHTML = 'name ';
+					document.getElementById('nameEqual').innerHTML = '= ';
+					document.getElementById('nameString').innerHTML = '"Kurt Robin Colonia" ';
+					document.getElementById('nameDelimit').innerHTML = 
+`; 
+		`;
+					
+				}
+
+				if(!document.getElementById('ageDelimit')) {
+					codeInsert.innerHTML += `<span class="code-keyword" id="ageKeyword"></span><span class="code-variable" id="ageVar"></span><span id="ageEqual"></span><span class="code-integer" id="ageInt"></span><span id="ageDelimit"></span>`;
+				}
+
+				if(document.getElementById('ageDelimit').innerHTML.trim() == '') {
+					clearTabContent([
+						'ageKeyword',
+						'ageVar',
+						'ageEqual',
+						'ageInt',
+						'ageDelimit'
+					]);
+					document.getElementById('ageKeyword').innerHTML = 'int ';
+					document.getElementById('ageVar').innerHTML = 'age ';
+					document.getElementById('ageEqual').innerHTML = '= ';
+					document.getElementById('ageInt').innerHTML = yearsDifference.toString();
+					document.getElementById('ageDelimit').innerHTML = 
+`;
+
+`;
+				}
+
+				if(!document.getElementById('aboutDelimiter')) {
+					codeInsert.innerHTML += `<span class="comment-question" id="aboutText"></span><span class="comment-question" id="aboutDelimiter"></span>`;
+				}
+
+				if(document.getElementById('aboutDelimiter').innerHTML.trim() == '') {
+					clearTabContent(['aboutText', 'aboutDelimiter'])
+					document.getElementById('aboutText').innerHTML = 
+`		// ? I'm an aspiring Full Stack Developer based in Quezon City, Philippines.
+		// ? I've recently recently graduated Bachelor of Science in Information Technology from New Era University - Main Campus. 
+		// ? I've worked on Web Apps utilizing the Laravel Framework, both Frontend and Backend, however my skills lean more towards Backend development
+		// ? I have a keen attention to detail, adaptable and am a fast learner, with analytical and problem solving skills`;
+					document.getElementById('aboutDelimiter').innerHTML =
+`.
+		
+		`;
+				}
+
+				if(!document.getElementById('tsDelimiter')) {
+					var techStack = `<span class="comment-question" id="tsSofDevSection"></span>`;
+					techStack += `<span class="code-string" id="tsJava"></span><span id="tsJavaComma"></span>`;
+					techStack += `<span class="code-string" id="tsKotlin"></span><span id="tsKotlinComma"></span>`;
+					techStack += `<span class="code-string" id="tsPython"></span><span id="tsPythonComma"></span>`;
+					techStack += `<span class="code-string" id="tsCSharp"></span><span id="tsCSharpComma"></span>`;
+					techStack += `<span class="comment-question" id="tsWebDevSection"></span>`;
+					techStack += `<span class="code-string" id="tsHtml"></span><span id="tsHtmlComma"></span>`;
+					techStack += `<span class="code-string" id="tsCss"></span><span id="tsCssComma"></span>`;
+					techStack += `<span class="code-string" id="tsJs"></span><span id="tsJsComma"></span>`;
+					techStack += `<span class="code-string" id="tsPhp"></span><span id="tsPhpComma"></span>`;
+					techStack += `<span class="code-string" id="tsRestApi"></span><span id="tsRestApiComma"></span>`;
+					techStack += `<span class="comment-question" id="tsDatabaseSection"></span>`;
+					techStack += `<span class="code-string" id="tsMySql"></span><span id="tsMySqlComma"></span>`;
+					techStack += `<span class="code-string" id="tsPostgres"></span><span id="tsPostgresComma"></span>`;
+					techStack += `<span class="code-string" id="tsFirebase"></span><span id="tsFirebaseComma"></span>`;
+					techStack += `<span class="code-string" id="tsDbeaver"></span><span id="tsDbeaverComma"></span>`;
+					techStack += `<span class="comment-question" id="tsFrameworkToolSection"></span>`;
+					techStack += `<span class="code-string" id="tsComposer"></span><span id="tsComposerComma"></span>`;
+					techStack += `<span class="code-string" id="tsLaravel"></span><span id="tsLaravelComma"></span>`;
+					techStack += `<span class="code-string" id="tsNodeJs"></span><span id="tsNodeJsComma"></span>`;
+					techStack += `<span class="code-string" id="tsJquery"></span><span id="tsJqueryComma"></span>`;
+					techStack += `<span class="code-string" id="tsReactJs"></span><span id="tsReactJsComma"></span>`;
+					techStack += `<span class="code-string" id="tsTailwind"></span><span id="tsTailwindComma"></span>`;
+					techStack += `<span class="code-string" id="tsBootstrap"></span><span id="tsBootstrapComma"></span>`;
+					techStack += `<span class="comment-question" id="tsOpSysSection"></span>`;
+					techStack += `<span class="code-string" id="tsWindows"></span><span id="tsWindowsComma"></span>`;
+					techStack += `<span class="code-string" id="tsLinux"></span><span id="tsLinuxComma"></span>`;
+					techStack += `<span class="comment-question" id="tsVersionControlSection"></span>`
+					techStack += `<span class="code-string" id="tsGit"></span><span id="tsGitComma"></span>`;
+					techStack += `<span class="code-string" id="tsGithub"></span><span id="tsGithubComma"></span>`;
+
+					
+					// * the end is never the end is never the end is never the end is never the end -> this line probably.
+					codeInsert.innerHTML += '<span class="code-keyword" id="tsKeyword"></span><span id="tsKeyArr"></span><span class="code-variable" id="tsVar"></span><span id="tsEqual"></span><span id="tsOpenBracket"></span>'+ techStack +'<span id="tsCloseBracket"></span><span id="tsDelimiter"></span>';
+					console.log(codeInsert.innerHTML)
+				}
+
+				if(document.getElementById('tsDelimiter').innerHTML.trim() == '') {
+					clearTabContent([
+						'tsKeyword',
+						'tsKeyArr',
+						'tsVar',
+						'tsEqual',
+						'tsOpenBracket',
+							'tsSofDevSection',
+								'tsJava', 'tsJavaComma',
+								'tsKotlin', 'tsKotlinComma',
+								'tsPython', 'tsPythonComma',
+								'tsCSharp', 'tsCSharpComma',
+							'tsWebDevSection',
+								'tsHtml', 'tsHtmlComma',
+								'tsCss', 'tsCssComma',
+								'tsJs', 'tsJsComma',
+								'tsPhp', 'tsPhpComma',
+								'tsRestApi', 'tsRestApiComma',
+							'tsDatabaseSection',
+								'tsMySql', 'tsMySqlComma',
+								'tsPostgres', 'tsPostgresComma',
+								'tsFirebase', 'tsFirebaseComma',
+								'tsDbeaver', 'tsDbeaverComma',
+							'tsFrameworkToolSection',
+								'tsComposer', 'tsComposerComma',
+								'tsLaravel', 'tsLaravelComma',
+								'tsNodeJs', 'tsNodeJsComma',
+								'tsJquery', 'tsJqueryComma',
+								'tsReactJs', 'tsReactJsComma',
+								'tsTailwind', 'tsTailwindComma',
+								'tsBootstrap', 'tsBootstrapComma',
+							'tsOpSysSection',
+								'tsWindows', 'tsWindowsComma',
+								'tsLinux', 'tsLinuxComma',
+							'tsVersionControlSection',
+								'tsGit', 'tsGitComma',
+								'tsGithub', 'tsGithubComma',
+						'tsCloseBracket',
+						'tsDelimiter'
+					]);
+					document.getElementById('tsKeyword').innerHTML = 'String';
+					document.getElementById('tsKeyArr').innerHTML = '[] ';
+					document.getElementById('tsVar').innerHTML = 'myStack ';
+					document.getElementById('tsEqual').innerHTML = '= ';
+					document.getElementById('tsOpenBracket').innerHTML = '{';
+
+					document.getElementById('tsSofDevSection').innerHTML = `
+			// ? Software Development
+			`;
+					document.getElementById('tsJava').innerHTML = `"Java"`; document.getElementById('tsJavaComma').innerHTML = `, `;
+					document.getElementById('tsKotlin').innerHTML = `"Kotlin"`; document.getElementById('tsKotlinComma').innerHTML = `, `;
+					document.getElementById('tsPython').innerHTML = `"Python"`; document.getElementById('tsPythonComma').innerHTML = `, `;
+					document.getElementById('tsCSharp').innerHTML = `"C#"`; document.getElementById('tsCSharpComma').innerHTML = `, 
+			`;
+
+					document.getElementById('tsWebDevSection').innerHTML = `
+			// ? Web Development
+			`;
+					document.getElementById('tsHtml').innerHTML = `"HTML"`; document.getElementById('tsHtmlComma').innerHTML = `, `;
+					document.getElementById('tsCss').innerHTML = `"CSS"`; document.getElementById('tsCssComma').innerHTML = `, `;
+					document.getElementById('tsJs').innerHTML = `"JavaScript"`; document.getElementById('tsJsComma').innerHTML = `, `;
+					document.getElementById('tsPhp').innerHTML = `"PHP"`; document.getElementById('tsPhpComma').innerHTML = `, `;
+					document.getElementById('tsRestApi').innerHTML = `"Rest APIs and HTTP Methods"`; document.getElementById('tsRestApiComma').innerHTML = `, 
+			`;
+					
+					document.getElementById('tsDatabaseSection').innerHTML = `
+			// ? Database Management
+			`;
+					document.getElementById('tsMySql').innerHTML = `"MySQL"`; document.getElementById('tsMySqlComma').innerHTML = `, `;
+					document.getElementById('tsPostgres').innerHTML = `"PostgreSQL"`; document.getElementById('tsPostgresComma').innerHTML = `, `;
+					document.getElementById('tsFirebase').innerHTML = `"Firebase"`; document.getElementById('tsFirebaseComma').innerHTML = `, `;
+					document.getElementById('tsDbeaver').innerHTML = `"DBeaver"`; document.getElementById('tsDbeaverComma').innerHTML = `, 
+			`;
+					
+					document.getElementById('tsFrameworkToolSection').innerHTML = `
+			// ? Frameworks
+			`;
+					document.getElementById('tsLaravel').innerHTML = `"PHP Laravel"`; document.getElementById('tsLaravelComma').innerHTML = `, `;
+					document.getElementById('tsNodeJs').innerHTML = `"Node.JS"`; document.getElementById('tsNodeJsComma').innerHTML = `, `;
+					document.getElementById('tsJquery').innerHTML = `"jQuery and AJAX"`; document.getElementById('tsJqueryComma').innerHTML = `, `;
+					document.getElementById('tsReactJs').innerHTML = `"React.JS"`; document.getElementById('tsReactJsComma').innerHTML = `, `;
+					document.getElementById('tsTailwind').innerHTML = `"Tailwind CSS"`; document.getElementById('tsTailwindComma').innerHTML = `, `;
+					document.getElementById('tsBootstrap').innerHTML = `"CSS Bootstrap"`; document.getElementById('tsBootstrapComma').innerHTML = `, 
+			`;
+
+					document.getElementById('tsOpSysSection').innerHTML = `
+			// ? Operating Systems
+			`;
+					document.getElementById('tsWindows').innerHTML = `"Windows 10"`; document.getElementById('tsWindowsComma').innerHTML = `, `;
+					document.getElementById('tsLinux').innerHTML = `"Linux (Debian WSL and Kali Linux)"`; document.getElementById('tsLinuxComma').innerHTML = `, 
+			`;
+
+					document.getElementById('tsVersionControlSection').innerHTML = `
+			// ? Version Control
+			`;
+					document.getElementById('tsGit').innerHTML = `"Git (Git Bash)"`; document.getElementById('tsGitComma').innerHTML = `, `;
+					document.getElementById('tsGithub').innerHTML = `"Github"`; document.getElementById('tsGithubComma').innerHTML = `, 
+		`;
+
+					document.getElementById('tsCloseBracket').innerHTML = '}';
+					document.getElementById('tsDelimiter').innerHTML = ';';
+				}
+			}
+
+			if(isTyping) await aboutMeContent();
+			else aboutMeContentNoAnim();
 			break;
 		case "TechStack":
 			break;
