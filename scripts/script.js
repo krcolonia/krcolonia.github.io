e3da78e000eea8fe291acf506b6af9e2d960d6b9
krcolonia <krcolonia@gmail.com>
2025-11-03 15:56:17 +0800

    Updated how desktop window layering works, added icons to site taskbar when a window opens

--- scripts/script.js
+++ scripts/script.js
@@ -8,31 +8,32 @@
 
 // ? Mouse Click Sounds
 //#region
-var canClick = false;
+var canClick = false
 const leftClickSound = new Audio('../sounds/mouseleft.ogg')
 const rightClickSound = new Audio('../sounds/mouseright.ogg')
 
 document.addEventListener('click', () => {
 	if(canClick) {
-		leftClickSound.currentTime = 0;
-		leftClickSound.play();
+		leftClickSound.currentTime = 0
+		leftClickSound.play()
 	}
 })
 
 document.addEventListener('contextmenu', () => {
 	if(canClick) {
-		rightClickSound.currentTime = 0;
-		rightClickSound.play();
+		rightClickSound.currentTime = 0
+		rightClickSound.play()
 	}
-});
+})
 //#endregion
 
 // ? Draggable Window object
 //#region
-let activeWindows = [];
+let activeWindows = []
+const taskbarContainer = document.getElementById('taskbar-apps')
 
 class DraggableWindow {
-	static highestZ = 1000;
+	static highestZ = 1000
 
 	constructor(icon, title, content, width, height, scrollable = false) {
 		this.id = `window-${Date.now()}-${Math.random()}`
@@ -42,18 +43,18 @@ class DraggableWindow {
 
 	createWindow(icon, title, content, width, height, scrollable = false) {
 
-		let scrollStyle = '';
+		let scrollStyle = ''
 		if (scrollable) {
-			scrollStyle = ' overflow-y-auto';
+			scrollStyle = ' overflow-y-auto'
 		}
 
-		const windowElement = document.createElement('div');
+		const windowElement = document.createElement('div')
 		windowElement.id = this.id
 		windowElement.className = 'desktop-window'
-		windowElement.style.width = width + 'vw';
-		windowElement.style.height = height + 'vh';
-		windowElement.style.opacity = 0;
-		windowElement.style.zIndex = ++DraggableWindow.highestZ;
+		windowElement.style.width = width + 'vw'
+		windowElement.style.height = height + 'vh'
+		windowElement.style.opacity = 0
+		windowElement.style.zIndex = ++DraggableWindow.highestZ
 		windowElement.innerHTML = `
 			<div class="desktop-window-header p-0">
 				<div class="p-2 d-flex gap-2 justify-content-start align-items-center">
@@ -67,14 +68,35 @@ class DraggableWindow {
 			</div>
 			<div class="desktop-window-content flex-grow-1 p-4 ${scrollStyle}">${content}</div>
 		`
+		// let taskbarButton = `
+		// <button class="taskbar-icon border-0 h-100 p-2" id="taskbar-${icon}">
+		// 	<img src="./images/desktop-icons/${icon}.png" class="h-100">
+		// </button>`
 
-		activeWindows.push(title);
+		const taskbarButton = document.createElement('button')
+		taskbarButton.className = 'taskbar-icon border-0 h-100 p-2'
+		taskbarButton.id = `taskbar-${icon}`
+		taskbarButton.innerHTML = `<img src="./images/desktop-icons/${icon}.png" class="h-100">`
+		taskbarContainer.appendChild(taskbarButton)
+
+		activeWindows.push(title)
 		document.body.appendChild(windowElement)
 
 		const closeButton = windowElement.querySelector('.close-window')
 		closeButton.addEventListener('click', () => {
 			windowElement.remove()
-			activeWindows = activeWindows.filter(item => item !== title);
+			document.getElementById(`taskbar-${icon}`).remove()
+			activeWindows = activeWindows.filter(item => item !== title)
+
+			if (activeWindows === undefined || activeWindows.length == 0) {
+				DraggableWindow.highestZ = 1000
+			}
+		})
+
+		const taskbarIcon = document.getElementById(`taskbar-${icon}`)
+
+		taskbarIcon.addEventListener('mousedown', () => {
+			this.bringToFront()
 		})
 
 		const windowWidth = windowElement.offsetWidth
@@ -88,14 +110,16 @@ class DraggableWindow {
 		void windowElement.offsetHeight
 
 		setTimeout(() => {
-			windowElement.style.opacity = 100;
+			windowElement.style.opacity = 100
 		}, 50)
 
 		return windowElement
 	}
 
 	bringToFront() {
-		this.element.style.zIndex = ++DraggableWindow.highestZ
+		if (this.element.style.zIndex != DraggableWindow.highestZ) {
+			this.element.style.zIndex = ++DraggableWindow.highestZ
+		}
 	}
 
 	makeDraggable() {
@@ -104,11 +128,11 @@ class DraggableWindow {
 		let currentX, currentY, initialX, initialY
 
 		this.element.addEventListener('mousedown', () => {
-			this.bringToFront();
+			this.bringToFront()
 		})
 
 		header.addEventListener('mousedown', (e) => {
-			isDragging = true;
+			isDragging = true
 			initialX = e.clientX - this.element.offsetLeft
 			initialY = e.clientY - this.element.offsetTop
 
@@ -136,7 +160,7 @@ class DraggableWindow {
 		})
 
 		document.addEventListener('mouseup', () => {
-			isDragging = false;
+			isDragging = false
 		})
 	}
 }
@@ -145,39 +169,47 @@ class DraggableWindow {
 // ? Loading Screen
 //#region
 async function loadingScreen() {
-	const loadingElement = document.getElementById('loading-screen');
-	const loadingIcon = document.getElementById('loading-icon');
-	const loadingText = document.getElementById('loading-text');
+	const loadingElement = document.getElementById('loading-screen')
+	const loadingIcon = document.getElementById('loading-icon')
+	const loadingText = document.getElementById('loading-text')
 	const loadingPfp = document.getElementById('loading-pfp')
 
 	await new Promise(resolve => setTimeout(resolve, 5500))
 	
-	loadingPfp.classList.remove('opacity-0');
-	loadingPfp.classList.add('opacity-100');
+	loadingPfp.classList.remove('opacity-0')
+	loadingPfp.classList.add('opacity-100')
 
-	loadingIcon.classList.remove('opacity-100');
-	loadingIcon.classList.add('opacity-0');
+	loadingIcon.classList.remove('opacity-100')
+	loadingIcon.classList.add('opacity-0')
 
 	loadingText.innerText = "Welcome back, Kurt Colonia."
 
 	await new Promise(resolve => setTimeout(resolve, 1500))
 
-	loadingElement.classList.remove('opacity-100');
-	loadingElement.classList.add('opacity-0');
+	loadingElement.classList.remove('opacity-100')
+	loadingElement.classList.add('opacity-0')
 
 	await new Promise(resolve => setTimeout(resolve, 1000))
 
-	loadingElement.classList.remove('d-flex');
-	loadingElement.classList.add('d-none');
-	canClick = true;
+	loadingElement.classList.remove('d-flex')
+	loadingElement.classList.add('d-none')
+	canClick = true
+
+	// new DraggableWindow(
+	// 'guide',
+	// 'User Guide',
+	// guideContent,
+	// guideWidth,
+	// guideHeight
+	// )
 
 	new DraggableWindow(
-	'guide',
-	'User Guide',
-	guideContent,
-	guideWidth,
-	guideHeight
-	);
+	'scrapbook',
+	'About Me',
+	aboutContent,
+	aboutWidth,
+	aboutHeight
+	)
 }
 
 loadingScreen()
@@ -186,81 +218,65 @@ loadingScreen()
 // ? Taskbar Date and Time section
 //#region
 function startTime() {
-	const today = new Date();
-	var hours = today.getHours();
-	var minutes = today.getMinutes();
-	var seconds = today.getSeconds();
-	var ampm = hours >= 12 ? 'PM' : 'AM';
-	hours = hours % 12;
-	hours = hours ? hours : 12;
-	minutes = minutes < 10 ? '0'+minutes : minutes;
-	// seconds = seconds < 10 ? '0'+seconds: seconds;
-	// document.getElementById('taskbar-clock').innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
-	document.getElementById('taskbar-clock').innerHTML = hours + ':' + minutes + ' ' + ampm;
-	setTimeout(startTime, 1000);
+	const today = new Date()
+	var hours = today.getHours()
+	var minutes = today.getMinutes()
+	var seconds = today.getSeconds()
+	var ampm = hours >= 12 ? 'PM' : 'AM'
+	hours = hours % 12
+	hours = hours ? hours : 12
+	minutes = minutes < 10 ? '0'+minutes : minutes
+	document.getElementById('taskbar-clock').innerHTML = hours + ':' + minutes + ' ' + ampm
+	setTimeout(startTime, 1000)
 }
 
 startTime()
 
-document.getElementById('currentYear').textContent = new Date().getFullYear();
+document.getElementById('currentYear').textContent = new Date().getFullYear()
 //#endregion
 
 // ? Automatic redirect if on mobile screens
 //#region
 if (window.innerWidth < 768) {
-	window.location.replace('./portfolio');
+	window.location.replace('./portfolio')
 }
 //#endregion
 
 // ? Desktop App Content
 //#region
+
+// * User Guide (currently unused because I think it should go unused...? kept in case i change my mind aaaa)
 const guideContent = `
 <h3 class="fw-bold" align="center">Welcome to Kurt Colonia's Portfolio!</h3>
-<p class="m-0 p-0 h-100 w-100" style="text-indent: 25px; text-align: justify; text-justify: inter-word;">
+<p class="m-0 p-0 h-100 w-100" style="text-indent: 25px text-align: justify text-justify: inter-word">
 	This is the current iteration/redesign of my web portfolio! I wanted to make my portfolio look like a desktop environment to make my portfolio a bit more unique compared to other people's portfolio.<br><br>
 	If you'd like to view a more traditional style of web portfolio, <a href="./portfolio">Click here!</a>
 </p>
-`;
-const guideWidth = '70';
-const guideHeight = '65';
-document.getElementById('guide-icon').addEventListener('click', function() {
-	if(!activeWindows.includes('User Guide')) {
-		new DraggableWindow(
-			'guide',
-			'User Guide',
-			guideContent,
-			guideWidth,
-			guideHeight,
-		);
-	}
-});
+`
+const guideWidth = '70'
+const guideHeight = '65'
 
-document.getElementById('about-icon').addEventListener('click', function() {
-	if(!activeWindows.includes('About Me')) {
-		new DraggableWindow(
-			'scrapbook', 
-			'About Me', 
-			`
-			<div class="d-flex flex-row p-2 justify-content-center align-items-center gap-3" id="about-header">
-				<img src="./images/PFP.png" class="rounded-circle m-0 p-0" style="width: 15%; border: 2px solid black;">
-				<div class="d-flex flex-column mt-3">
-					<h3 class="fw-bold">Kurt Robin Colonia</h3>
-					<p>Full-Stack Web Developer</p>
-				</div>
-			</div>
-			<div id="about-content" class="p-3 m-0">
-				<p style="text-center">Hello World! I'm Kurt, a Full Stack Web Developer</p>
-			</div>
-			`,
-			'75',
-			'80',
-			true
-		);
-	}
-});
+// * About Me
+const aboutContent = `
+<div class="d-flex flex-row p-2 justify-content-center align-items-center gap-3" id="about-header">
+	<img src="./images/PFP.png" class="rounded-circle m-0 p-0" style="width: 15%; border: 2px solid black">
+	<div class="d-flex flex-column mt-3">
+		<h3 class="fw-bold">Kurt Robin Colonia</h3>
+		<p class="m-0 p-0">Full-Stack Web Developer</p>
+	</div>
+</div>
+<div id="about-content" class="p-3 m-0">
+	<p style="text-center">Hello World! I'm Kurt, a Full Stack Web Developer</p>
+</div>
+`
+const aboutWidth = '75'
+const aboutHeight = '80'
 
-let projectContent = ``;
-let projectFlex = ``;
+// * My Projects
+let projectCard = ``
+let projectContent = ``
+let projectWidth = '40'
+let projectHeight = '65'
 // ? github rest api for fetching my github repos
 fetch('https://api.github.com/users/krcolonia/repos')
 	.then(response => response.json())
@@ -269,7 +285,7 @@ fetch('https://api.github.com/users/krcolonia/repos')
 			const repos = ["JRSK-Booking", "CodeBreakers", "Yummly", "GameSRC", "Java_"]
 
 			if(repos.some(repo => item.name.includes(repo))) {
-				let homepage = ``;
+				let homepage = ``
 
 				let uploadDate = new Date(item.created_at).toLocaleDateString('en-US', {
 					month: 'long',
@@ -280,7 +296,7 @@ fetch('https://api.github.com/users/krcolonia/repos')
 				let language = item.language
 
 				if (language === "Blade") {
-					language = 'Laravel';
+					language = 'Laravel'
 				}
 
 				// ? I don't like nesting if statements, but my caveman brain can't currently think of a better solution
@@ -288,91 +304,120 @@ fetch('https://api.github.com/users/krcolonia/repos')
 					if (item.homepage.length != 0) {
 						homepage = `
 						<a href="${item.homepage}" target="_blank" class="github-card-button p-1">View Deployed Page</a>
-						`;
+						`
 					}
 				}
 
-				projectContent += `
+				projectCard += `
 					<div class="p-3 m-0 github-card" id="${item.name}">
-						<div class="d-flex flex-row justify-content-between m-0 p-0">
+						<div class="d-flex flex-column m-0 p-0">
 							<p class="text-xl fw-bold m-0 p-0">${item.name}</p>
-							<span id="github-date" class="fw-normal fst-italic p-0 m-0">Developed using ${language}</span>
+							<hr class="my-2 p-0">
+							<div class="d-flex flex-row justify-content-between mb-3">
+								<span class="github-details fw-normal fst-italic p-0 m-0">Uploaded ${uploadDate}</span>
+								<span class="github-details fw-normal fst-italic p-0 m-0">Developed using ${language}</span>
+							</div>
 						</div>
-						<hr class="m-2 p-0">
-						<p style="text-indent: 25px; text-align: justify;">${item.description}</p>
+						<p style="text-indent: 25px text-align: justify">${item.description}</p>
 						<div class="d-flex flex-row justify-content-between">
 							<a href="${item.html_url}" target="_blank" class="github-card-button p-1">Visit Repository</a>
 							${homepage}
 						</div>
 					</div>
-				`;
+				`
 
-				projectFlex = `
+				projectContent = `
 				<div class="d-flex flex-column gap-3 p-0 m-0">
-				${projectContent}
+				${projectCard}
 				</div>
-				`;
+				`
 			}
 		})
 	)
-	.catch(error => console.error('Error:', error));
+	.catch(error => console.error('Error:', error))
+
+// * Contact Me
+const contactContent = `
+<p class="m-0 p-0 w-100">content</p>
+`
+const contactWidth = '45'
+const contactHeight = '45'
+
+// * My Resume
+const resumeContent = `
+<embed src="./objects/Colonia_Resume.pdf" class="w-100 h-100" style="border-radius: 8px">
+`
+const resumeWidth = '50'
+const resumeHeight = '85'
+
+document.getElementById('about-icon').addEventListener('click', function() {
+	if(!activeWindows.includes('About Me')) {
+		new DraggableWindow(
+			'scrapbook', 
+			'About Me', 
+			aboutContent,
+			aboutWidth,
+			aboutHeight,
+			true
+		)
+	}
+})
 
 document.getElementById('project-icon').addEventListener('click', function() {
 	if(!activeWindows.includes('My Projects')) {
 		new DraggableWindow(
 			'projects', 
 			'My Projects', 
-			projectFlex,
-			'45',
-			'60',
+			projectContent,
+			projectWidth,
+			projectHeight,
 			true
-		);
+		)
 	}
-});
+})
 
 document.getElementById('contact-icon').addEventListener('click', function() {
 	if(!activeWindows.includes('Contact Me')) {
 		new DraggableWindow(
 			'contact', 
 			'Contact Me', 
-			`<p class="m-0 p-0 w-100">content</p>`,
-			'45',
-		);
+			contactContent,
+			contactWidth,
+			contactHeight
+		)
 	}
-});
+})
 
 document.getElementById('resume-icon').addEventListener('click', function() {
 	if(!activeWindows.includes('My Résumé')) {
 		new DraggableWindow(
 			'file', 
 			'My Résumé', 
-			`
-			<embed src="./objects/Colonia_Resume.pdf" class="w-100 h-100" style="border-radius: 8px;">
-			`,
-			'50',
-			'85'
-		);
+			resumeContent,
+			resumeWidth,
+			resumeHeight
+		)
 	}
-});
+})
 
 document.getElementById('linkedin-icon').addEventListener('click', function() {
 	window.open(
 		'https://www.linkedin.com/in/krcolonia/', 
 		'_blank'
-	);
+	)
 })
 
 document.getElementById('github-icon').addEventListener('click', function() {
 	window.open(
 		'https://github.com/krcolonia', 
 		'_blank'
-	);
+	)
 })
 
 document.getElementById('gmail-icon').addEventListener('click', function() {
 	window.open(
 		'mailto:krcolonia@gmail.com', 
 		'_blank'
-	);
+	)
 })
 //#endregion
